e39921d64c7db6e65324b0bf783121960b58d2ab
tommy-lariccia <90263608+tommy-lariccia@users.noreply.github.com>
2022-05-19 09:56:12 -0400

    Create donne-game.mjs

---
+++ donneJSProject/donne-game.mjs
@@ -0,0 +1,19 @@
+import MainScene from './MainScene.mjs'
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 800,
+    scale: 2,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 600 },
+            debug: false
+        }
+    },
+    scene: [MainScene]
+};
+
+const game = new Phaser.Game(config);
